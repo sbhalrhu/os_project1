323ff8f43f83982cdc7fbe4c7148f6a2539408c3
sbhalrhu <sahibbhalrhu@gmail.com>
2026-09-11 00:55:53 -0700

    possible error?

--- two.c
+++ two.c
@@ -2,18 +2,14 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
-
 int main(int argc, char *argv[])
-{
+   {
    char buff[1024];
    int cnt, i;
    struct timeval tv;
-
    tv.tv_usec = 0;
    tv.tv_sec = 1;
-
    cnt = atol(argv[1]);
-
    sprintf(buff, "%%%dd\n", 8*cnt);
    for(i = 0; i < cnt; i++) {
       printf(buff, cnt);
@@ -21,4 +17,4 @@ int main(int argc, char *argv[])
       sleep(1);
    }
    return 0;
-}
+}  
