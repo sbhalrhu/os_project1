61349d6d4f37d66a6e6c54d3113d5fd8397be5e7
sbhalrhu <sahibbhalrhu@gmail.com>
2026-09-11 01:02:15 -0700

    added path modification to remove ./ requirement

--- schedule.c
+++ schedule.c
@@ -175,6 +175,20 @@ int main(int argc, char *argv[]) {
      * The quantum is common among all processes.
      */
 
+
+    /* TO BE ABLE to run two.c as two not ./two, we need to modify the directory and path environments.
+     * 
+    */
+    char *old_path = getenv("PATH");
+    if (old_path) {
+        char *new_path = malloc(strlen(old_path) + 3); 
+        sprintf(new_path, ".:%s", old_path);
+        setenv("PATH", new_path, 1);
+        free(new_path);
+    } else {
+        setenv("PATH", ".", 1);
+    }
+
     int quantum = atoi(argv[1]);
     int current_arg = 2;
 
